bb87933b4c0cde25efc92a8aedcc2685be250637
Feather <lisafeather@gmail.com>
2013-11-16 16:40:57 -0800

    made it say hello world

--- express_app.js
+++ express_app.js
@@ -1,4 +1,15 @@
-var express = require('express');
+var express = 	require('express'),
+	passport =  require('passport');
+
 var app = express();
  
 app.listen(3000);
+
+app.get('/', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.send(body);
+});
+
+console.log("hello world");
